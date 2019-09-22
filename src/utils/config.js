601a15252b5b0d5879de6d9f7725bb39c854c5eb
rifai <inoreds@gmail.com>
2019-09-22 15:38:10 +0700

    [update] change api endpoint

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,7 @@
 module.exports = {
     app_version: '0.1.1-1',
     base_routing: '/',
-    api_endpoint: 'http://localhost:8000/api/administration/', // staging
-    // api_endpoint_uplooad: 'https://nusaindotech.com/ocr_api/', // staging
-
+    api_endpoint: 'http://vtiapi.partnertema.com/api/administration/', // staging
     colors: {
         red: '#C62828',
         white: '#fffcfc',
